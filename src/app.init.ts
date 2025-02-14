d11e7d6954880768de95623798cf414770d88fac
R3pic <101osc@naver.com>
2025-02-14 16:54:43 +0900

    cors 문제 해결

--- src/app.init.ts
+++ src/app.init.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import studentController from "@student/student.controller.ts";
 import entertainmentController from "@/entertainment/entertainment.controller.ts";
@@ -8,6 +9,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use('/entertainments', entertainmentController);
 app.use('/students', studentController);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
